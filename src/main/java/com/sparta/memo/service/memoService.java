c1d8b18d48b5e32844aa41b017e00db652d2aeba
harrisbang2 <harrisbang98@gmail.com>
2024-01-20 19:04:10 +0900

    controller/service/repository + IoC and DI

---
+++ src/main/java/com/sparta/memo/service/memoService.java
@@ -0,0 +1,47 @@
+package com.sparta.memo.service;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.entity.Memo;
+import com.sparta.memo.memoRepositary.memoRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class memoService {
+    private final JdbcTemplate jdbcTemplate;
+    private final memoRepository repo; // repository
+    public memoService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        repo = new memoRepository(jdbcTemplate); // repository
+    }
+
+    public MemoResponseDto createMemo(MemoRequestDto requestDto) { // create memo service
+        Memo memo = new Memo(requestDto);
+        // DB 저장
+        KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본 키를 반환받기 위한 객체
+        memo = repo.createkeyHolder(memo,keyHolder);
+        // Entity -> ResponseDto
+        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+        return memoResponseDto;
+    }
+
+    public List<MemoResponseDto> getMemos() {
+        return repo.repoGetAllMemo();
+    }
+
+    public Long putMemo(long id, MemoRequestDto requestDto) {
+        return repo.putMemo(requestDto,id);
+    }
+    public Long deleteMemo(long id) {
+        // 해당 메모가 DB에 존재하는지 확인
+        return repo.deleteMemo(id);
+    }
+}
